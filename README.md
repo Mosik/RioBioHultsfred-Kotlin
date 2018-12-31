6445b32371086872976daf453463fe905f56e676
Sebastian <oslobar@gmail.com>
2018-12-31 20:19:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# RioBioHultsfred-Kotlin
---
+++ app/src/main/java/inc/intima/riobiohultsfred/MainActivity.kt
@@ -0,0 +1,111 @@
+package inc.intima.riobiohultsfred
+
+import android.graphics.Bitmap
+import android.os.Build
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val url = "https://riobiohultsfred.se"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Get the web view settings instance
+        val settings = webview.settings
+
+        // Enable java script in web view
+        settings.javaScriptEnabled = true
+
+        // Enable and setup web view cache
+        settings.setAppCacheEnabled(true)
+        settings.cacheMode = WebSettings.LOAD_DEFAULT
+        settings.setAppCachePath(cacheDir.path)
+
+
+        // Enable zooming in web view
+        settings.setSupportZoom(false)
+        settings.builtInZoomControls = false
+        settings.displayZoomControls = false
+
+
+        // Zoom web view text
+        //settings.textZoom = 125
+
+
+        // Enable disable images in web view
+        settings.blockNetworkImage = false
+        // Whether the WebView should load image resources
+        settings.loadsImagesAutomatically = true
+
+
+        // More web view settings
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            settings.safeBrowsingEnabled = true  // api 26
+        }
+        //settings.pluginState = WebSettings.PluginState.ON
+        settings.useWideViewPort = true
+        settings.loadWithOverviewMode = true
+        settings.javaScriptCanOpenWindowsAutomatically = true
+{
+            settings.mediaPlaybackRequiresUserGesture = false
+        }
+
+
+        // More optional settings, you can enable it by yourself
+        settings.domStorageEnabled = true
+        settings.setSupportMultipleWindows(true)
+        settings.loadWithOverviewMode = true
+        settings.allowContentAccess = true
+        settings.setGeolocationEnabled(true)
+
+
+
+        settings.allowFileAccess = true
+
+        // WebView settings
+        webview.fitsSystemWindows = true
+
+        /* if SDK version is greater of 19 then activate hardware acceleration
+        otherwise activate software acceleration  */
+
+        webview.setLayerType(View.LAYER_TYPE_HARDWARE, null)
+
+        webview.loadUrl(url)
+
+        // Set web view client
+        webview.webViewClient = object : WebViewClient() {
+            override fun onPageStarted(view: WebView, url: String, favicon: Bitmap?) {
+                // Page loading started
+                // Do something
+            }
+
+            override fun onPageFinished(view: WebView, url: String) {
+                // Page loading finished
+                // Enable disable back forward button
+            }
+        }
+
+    }
+
+
+
+    override fun onBackPressed() {
+        if (webview.canGoBack()) {
+            // If web view have back history, then go to the web view back history
+            webview.goBack()
+        }
+    }
+}
+
+
+
+
+
